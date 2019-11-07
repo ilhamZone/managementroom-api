017302dfa31c551079f86f0987e1ef228e8afe9c
unknown <iam.mhd07@gmail.com>
2019-11-07 08:50:20 +0700

    update model

--- migrations/1-create-user.js
+++ migrations/1-create-user.js
@@ -14,6 +14,9 @@ module.exports = {
       password: {
         type: Sequelize.STRING
       },
+      image: {
+        type: Sequelize.STRING
+      },
       name: {
         type: Sequelize.STRING
       },
--- seeders/1-users.js
+++ seeders/1-users.js
@@ -6,18 +6,15 @@ module.exports = {
       {
         email: 'palay@tes.com',
         password: 'palay007',
+        image: 'https://d1bvpoagx8hqbg.cloudfront.net/259/1049b67254cf59b868cd1ea12213cd1e.jpg',
         name: 'Ilham Muhammad',
       },
       {
         email: 'budi@tes.com',
         password: 'palay007',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Pierre-Person.jpg/220px-Pierre-Person.jpg',
         name: 'Budi Santoso',
-      },
-      {
-        email: 'tono@tes.com',
-        password: 'palay007',
-        name: 'Tono Sukirman',
-      },
+      }
     ], {});
   },
 
